b18fb2ac80d0a6a19019851efc6ba9999fff214d
jhonthe7 <jhonthe7@gmail.com>
2025-08-07 22:43:21 -0500

    Ajusta el cálculo del valor máximo en las estadísticas de hábitos para mejorar la visualización

--- app.py
+++ app.py
@@ -305,6 +305,6 @@ elif menu == "📈 Estadísticas":
                 plot_bgcolor="rgba(0,0,0,0)"
             )
             fig_actividades.update_traces(textposition="outside")
-            max_val = df["Hábito"].count()
+            max_val = df["Hábito"].nunique()*1.5
             fig_actividades.update_yaxes(range=[0, max_val * 1.5])
             st.plotly_chart(fig_actividades, use_container_width=True)
